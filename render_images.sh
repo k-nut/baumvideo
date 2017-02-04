6db55f44354e494df08bcb23c1a18880acb3d095
Knut Hühne <knut@k-nut.eu>
2017-02-04 21:24:42 +0100

    use nik4 instead of mapnik-render
    This allows to create images in a bigger resolution

--- render_images.sh
+++ render_images.sh
@@ -1,8 +1,7 @@
-cd rendered
-rm * 
-cd ../mapnik
+rm rendered/* 
 
 for i in {1880..2016}
 do
-  ./utils/mapnik-render/mapnik-render ../styles/$i.xml ../rendered/$i.png
+  nik4.py -b 13.08 52.31 13.77 52.70 -z 11 styles/$i.xml rendered/$i.png
+  echo "rendered $i"
 done
