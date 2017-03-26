686b758f4c71cde8568b7334b6329a3e4d4c7f16
Knut Hühne <knut@k-nut.eu>
2017-03-26 10:47:33 +0200

    do not hardcode path in template

--- create_styles.sh
+++ create_styles.sh
@@ -1,7 +1,8 @@
 mkdir -p styles
 rm styles/* || true
+#ESCAPED_PATH=$(pwd | sed "s/\//\\\/")
 
 for i in {1880..2016}
 do
-  sed "s/YEAR/$i/" template.xml > styles/$i.xml
+  sed "s:PATH:$(pwd):g" template.xml | sed -e "s/YEAR/$i/" > styles/$i.xml
 done
